89f63087388bd1b112b675ee5d381181b62f2432
Mahbub049 <88799698+Mahbub049@users.noreply.github.com>
2022-12-18 03:14:40 +0600

    added categories and data stored in databased 02+03

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\CategoryController;
 
 
 /*
@@ -25,6 +26,9 @@ Route::get('/admins',[AdminController::class, 'index']);
 Route::get('/dashboard',[SuperAdminController::class, 'dashboard']);
 Route::post('//admin-dashboard',[AdminController::class, 'show_dashboard']);
 Route::get('/logout',[SuperAdminController::class, 'logout']);
+
+//Category Routes
+Route::resource('/categories/', CategoryController::class);
 
 
 // Frontend route
